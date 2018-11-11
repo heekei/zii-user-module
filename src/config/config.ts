24a7632f51f9c93ca00834698ac101cde06ba5ae
Heekei <zxq0610@qq.com>
2018-11-11 23:52:56 +0800

    untrack

--- src/config/config.ts
+++
@@ -1,8 +0,0 @@
-export default {
-    dbHost: 'localhost',
-    dbPort: 27017,
-    dbName: 'zii_users',
-    dbUri:'mongodb://localhost:27017/zii_users',
-    username: 'xxx',
-    password: 'xxx'
-}
